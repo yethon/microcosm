807e21a74672f89fe2dfcec78ec600e81632d2b0
yethon <ursula.yethon@mheducation.com>
2015-10-08 20:23:46 -0400

    Fix watch gulp task

--- tasks/watch.js
+++ tasks/watch.js
@@ -9,7 +9,7 @@ function reportChange(event) {
 // automatically reload browsers when an asset changes
 gulp.task('watch', function () {
   gulp.watch(['assets/styles/**.css', 'assets/styles/**.scss'], ['styles']).on('change', reportChange);
-  gulp.watch('assets/js/**.js', ['javascript']).on('change', reportChange);
+  gulp.watch('assets/js/**.js', ['scripts', browserSync.reload]).on('change', reportChange);
   gulp.watch('index.html', ['copy-index']).on('change', reportChange);
   gulp.watch(['./dist/index.html', './dist/assets/img/**.*'], browserSync.reload);
   gulp.watch('assets/img/**.*', ['copy-images']).on('change', reportChange);
