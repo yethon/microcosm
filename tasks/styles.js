78baa30f4acb229f624cfbe4643d1f211c5645c9
yethon <ursula.yethon@mheducation.com>
2015-10-16 16:30:07 -0400

    Handful of minor updates

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,14 +1,19 @@
-var gulp        = require('gulp');
-var concatCss   = require('gulp-concat-css');
-var minifyCSS   = require('gulp-minify-css');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
+var gulp         = require('gulp');
+var concatCss    = require('gulp-concat-css');
+var minifyCSS    = require('gulp-minify-css');
+var browserSync  = require('browser-sync');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // compile scss files and concat all subsequent css
 // minify css and copy to 'dist' folder
 gulp.task('styles', function () {
   return gulp.src(['assets/styles/**.css', 'assets/styles/**.scss'])
   .pipe(sass())
+  .pipe(autoprefixer( {
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
   .pipe(concatCss('site.min.css'))
   .pipe(minifyCSS({debug: true}))
   .pipe(gulp.dest('./dist/assets'))
